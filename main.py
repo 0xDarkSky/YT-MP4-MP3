798f0157ff6947cdfc88a0f35aeed5ae848bf5e3
QL0R <84932430+QL0R@users.noreply.github.com>
2021-10-20 16:10:17 +0000

    Added a function
    Now you can download yt playlists too

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import time
 
 try:
     import PySimpleGUI as sg
-    from pytube import YouTube
+    from pytube import YouTube, Playlist
+    import re
 except ImportError:
     os.system("pip install pysimplegui")
     os.system("pip install pytube")
 
 def main():
 
-    sg.theme('DarkAmber') 
-
-    right_click_menu = ["", ["Copy", "Paste", "Select All", "Cut"]]
-    MLINE_KEY = "-MLINE-"
+    sg.theme('DarkAmber')
 
     layout = [  [sg.Text("Enter the URL"), sg.InputText()],
                 [sg.Text("Enter the PATH"), sg.InputText()],
@@ -28,19 +26,35 @@ def main():
            window.close()
            break
 
-        if event == sg.WIN_CLOSED or event == "Cancel": 
+        if event == sg.WIN_CLOSED or event == "Cancel":
            exit()
 
     window.close()
 
-    yt = YouTube(str(values[0]))
-    video = yt.streams.filter(only_audio=True).first()
-    destination = str((values[1]))
-    out_file = video.download(output_path=destination)
+    if "playlist" in str(values[0]): # detects if you're trying to download a playlist
 
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
+        playlist = Playlist(str(values[0]))
+
+        playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+
+        for url in playlist.video_urls:
+            yt = YouTube(url)
+            video = yt.streams.filter(only_audio=True).first()
+            destination = str((values[1]))
+            out_file = video.download(output_path=destination)
+
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
+    else:
+        yt = YouTube(str(values[0]))
+        video = yt.streams.filter(only_audio=True).first()
+        destination = str((values[1]))
+        out_file = video.download(output_path=destination)
+
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
 
     newLayout = [ [sg.Text(f"The .mp3 is downloaded in {destination}")],
     [sg.Button("Exit")], [sg.Button("Download")] ]
@@ -58,7 +72,7 @@ def main():
            newWindow.close()
            main()
 
-        if event == sg.WIN_CLOSED or event == "Cancel": 
+        if event == sg.WIN_CLOSED or event == "Cancel":
            exit()
 
     newWindow.close()
